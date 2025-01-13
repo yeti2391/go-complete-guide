4de7b9b623e106e6b5c060d4e378b8e69e38ed5e
Marco González <63669446+yeti2391@users.noreply.github.com>
2025-01-13 13:02:33 -0300

    Create ejercicio02-profit-calculator-with-functions.go

---
+++ Seccion 02 Go Essentials/ejercicio02-profit-calculator-with-functions.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+//se re hace utilizando funciones
+
+func main(){
+	// datos a solicitar al usuario
+	fmt.Print("Ingrese los ingresos: ")
+	revenue := obtenerDatos() //revisar
+	
+	fmt.Print("Ingrese los costos: ")
+	expenses := obtenerDatos()
+	
+	fmt.Print("Ingrese los impuestos: ")
+	tax_rate := obtenerDatos()
+
+	calculando(revenue, expenses, tax_rate) 
+}
+ 
+//se crea una única función para pedir input del usuario
+func obtenerDatos() float64{
+	var userInput float64
+	fmt.Scan(&userInput)
+	return userInput
+ }
+
+
+//se crea una única función para realizar los calculos e imprimir resultados
+func calculando(userRevenue, userExpenses, userTax_rate float64){
+	//calculo de ganancias sin impuestos
+	ebt := userRevenue-userExpenses
+	fmt.Printf("Las ganancias antes de impuestos (EBT) es de: %.2f\n", ebt)
+
+	//calculo de ganancias después de los ingresos
+	profit := ebt * (1 - userTax_rate / 100)
+	fmt.Printf("Después de los impuestos las ganancias son: %.2f\n",profit)
+
+	//calculo de ratio
+	ratio := ebt/profit
+	fmt.Printf("El calculo de ratio es de: %.2f\n", ratio)
+ }
+
+
+/*
+"calculadora de ganancias" en Go. 
+Esta calculadora tomará como entrada los ingresos (revenue), los gastos (expenses) y la tasa de impuestos (tax rate), 
+y calculará las ganancias antes de impuestos (EBT) y las ganancias después de impuestos (profit), y el ratio (EBT / profit)
+
+*/
